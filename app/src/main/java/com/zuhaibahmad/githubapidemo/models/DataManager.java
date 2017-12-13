81ef5a09f792c1d0b1c9557c35c43e0a5287507a
Zuhaib Ahmad <xuhaib_ahmad@yahoo.com>
2017-12-13 16:28:27 +0500

    --allow-empty

--- app/src/main/java/com/zuhaibahmad/githubapidemo/models/DataManager.java
+++ app/src/main/java/com/zuhaibahmad/githubapidemo/models/DataManager.java
@@ -2,6 +2,10 @@ package com.zuhaibahmad.githubapidemo.models;
 
 import com.zuhaibahmad.githubapidemo.data.Contributor;
 
+import java.util.List;
+
+import io.reactivex.Single;
+
 /**
  * Created by Zuhaib on 12/13/2017.
  */
